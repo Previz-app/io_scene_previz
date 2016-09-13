7afaa008a5a43bda3a61d844559002c72c4f101e
Charles Flèche <charles.fleche@free.fr>
2016-09-13 12:11:26 +0200

    Do not request io_three anymore during tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,4 +2,4 @@
 
 TESTS_DIR=`dirname ${BASH_SOURCE[0]}`
 
-blender --factory-startup --debug --background --addons io_three,io_scene_dnb_previz --python $TESTS_DIR/run_tests.py
+blender --factory-startup --debug --background --addons io_scene_dnb_previz --python $TESTS_DIR/run_tests.py
