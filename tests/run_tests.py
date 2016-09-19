e534b87f8483783eeb7cc74026d02e55fe1570f6
Charles Flèche <charles.fleche@free.fr>
2016-09-19 20:17:26 +0200

    Moved test environment checks in run_tests.py

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,6 +2,9 @@ import os
 from pathlib import Path
 import sys
 
+
+# Add path for modules installed by pip
+
 if 'VIRTUAL_ENV' in os.environ:
     major, minor = sys.version_info.major, sys.version_info.minor
     python = 'python{major}.{minor}'.format(major=major, minor=minor)
@@ -12,14 +15,44 @@ if 'VIRTUAL_ENV' in os.environ:
 import coverage
 import nose
 
-def main():
-    python_path = str(Path(__file__).parent.parent)
-    sys.path.append(python_path)
-    nose.run(argv=[__file__])
 
-if __name__ == '__main__':
+# Add blender repo path for tests module
+
+blender_addon_repo_root_path = str(Path(__file__).parent.parent)
+sys.path.append(blender_addon_repo_root_path)
+
+from tests.utils import PREVIZ_API_ROOT_ENVVAR, PREVIZ_API_TOKEN_ENVVAR
+
+
+def error_message():
+    return '\n'.join(
+        [
+            '\n',
+            78*'*',
+            'Testing environment not set. Set the variables :',
+            '\t-{}'.format(PREVIZ_API_ROOT_ENVVAR),
+            '\t-{}'.format(PREVIZ_API_TOKEN_ENVVAR),
+            78*'*',
+            '\n'
+        ]
+    )
+
+def is_environment_valid():
+    def envvar_is_valid(envvar):
+        return len(os.environ.get(envvar, '')) > 0
+    
+    return envvar_is_valid(PREVIZ_API_ROOT_ENVVAR) \
+       and envvar_is_valid(PREVIZ_API_TOKEN_ENVVAR)
+
+def main():
     cov = coverage.coverage()
     cov.start()
-    main()
+    nose.run(argv=[__file__])
     cov.stop()
     cov.save()
+
+if __name__ == '__main__':
+    if not is_environment_valid():
+        print(error_message())
+        raise EnvironmentError
+    main()
