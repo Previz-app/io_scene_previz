b5b4a4c8c36381bad6da5cdd93f4cae1929cd952
Charles Flèche <charles.fleche@free.fr>
2016-09-19 17:01:04 +0200

    Added tox / nose / coverage

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,8 +1,25 @@
-import pathlib
+import os
+from pathlib import Path
 import sys
 
-python_path = str(pathlib.Path(__file__).parent.parent)
-sys.path.append(python_path)
+if 'VIRTUAL_ENV' in os.environ:
+    major, minor = sys.version_info.major, sys.version_info.minor
+    python = 'python{major}.{minor}'.format(major=major, minor=minor)
+    envroot = Path(os.environ['VIRTUAL_ENV'])
+    env_site_packages = envroot / 'lib' / python / 'site-packages'
+    sys.path.append(str(env_site_packages))
 
+import coverage
+import nose
 
-import tests.__main__
+def main():
+    python_path = str(Path(__file__).parent.parent)
+    sys.path.append(python_path)
+    nose.run(argv=[__file__])
+
+if __name__ == '__main__':
+    cov = coverage.coverage()
+    cov.start()
+    main()
+    cov.stop()
+    cov.save()
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,3 +3,4 @@
 TESTS_DIR=`dirname ${BASH_SOURCE[0]}`
 
 blender --factory-startup --debug --background --addons io_scene_dnb_previz --python $TESTS_DIR/run_tests.py
+
