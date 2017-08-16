4e747a4f63f3366c6213eb385b28900f27fa1605
Charles Flèche <charles@previz.co>
2017-08-16 16:23:57 +0200

    Add task remove UI

--- io_scene_previz/progress.py
+++ io_scene_previz/progress.py
@@ -1,3 +1,5 @@
+import datetime
+
 import addon_utils
 import bpy
 from bpy.app.handlers import persistent
@@ -22,6 +24,13 @@ class TasksRunner(object):
         self.tasks[id] = task
         return id
 
+    def remove_task(self, task_id):
+        task = self.tasks[task_id]
+        if not task.is_finished:
+            msg = 'Cannot remove unfinished task {!r}'.format(task.label)
+            raise RunTimeError(msg)
+        del self.tasks[task_id]
+
     def run(self):
         pass
 
@@ -43,12 +52,20 @@ class Task(object):
         self.state = 'state'
         self.error = None
         self.progress = None
+        self.finished_time = None
+        self.is_cancellable = True
 
     def run(self):
         pass
 
     def cancel(self):
-        pass
+        self.finished_time = datetime.datetime.now()
+        self.state = 'Cancelled'
+        self.status = CANCELLED
+
+    @property
+    def is_finished(self):
+        return self.status in (DONE, CANCELLED, ERROR)
 
     def tick(self):
         pass
@@ -75,6 +92,22 @@ class CancelTask(bpy.types.Operator):
 
     def execute(self, context):
         self.report({'INFO'}, 'Previz: Cancel task {}'.format(self.task_id))
+        tasks_runner.tasks[self.task_id].cancel()
+        return {'FINISHED'}
+
+
+class RemoveTask(bpy.types.Operator):
+    bl_idname = 'export_scene.previz_remove_task'
+    bl_label = 'Remove Previz task'
+
+    task_id = IntProperty(
+        name = 'Task ID',
+        default = -1
+    )
+
+    def execute(self, context):
+        self.report({'INFO'}, 'Previz: Remove task {}'.format(self.task_id))
+        tasks_runner.remove_task(self.task_id)
         return {'FINISHED'}
 
 
@@ -93,16 +126,29 @@ class Panel(bpy.types.Panel):
         for id, task in tasks_runner.tasks.items():
             row = self.layout.row()
             row.label('{} ({})'.format(task.label, task.state))
-            row.operator('export_scene.previz_cancel_task', text='', icon='CANCEL').task_id = id
+
+            if task.is_cancellable and not task.is_finished:
+                row.operator(
+                    'export_scene.previz_cancel_task',
+                    text='',
+                    icon='CANCEL').task_id = id
+
+            if task.is_finished:
+                row.operator(
+                    'export_scene.previz_remove_task',
+                    text='',
+                    icon='X').task_id = id
 
 
 def register():
     bpy.utils.register_class(Test)
     bpy.utils.register_class(CancelTask)
+    bpy.utils.register_class(RemoveTask)
     bpy.utils.register_class(Panel)
 
 
 def unregister():
     bpy.utils.unregister_class(Test)
     bpy.utils.unregister_class(CancelTask)
+    bpy.utils.unregister_class(RemoveTask)
     bpy.utils.unregister_class(Panel)
