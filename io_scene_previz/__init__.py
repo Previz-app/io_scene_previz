a57601088bceed69a9f9061909b8c033608e5ceb
Charles Flèche <charles@previz.co>
2017-08-22 16:15:44 +0200

    Bump version: 1.0.4 → 1.1.0

--- io_scene_previz/__init__.py
+++ io_scene_previz/__init__.py
@@ -35,7 +35,7 @@ from . import utils
 bl_info = {
     'name': "Previz integration",
     'author': "Previz (info@previz.co)",
-    'version': (1, 0, 4),
+    'version': (1, 1, 0),
     'blender': (2, 76, 0),
     'location': "File > Export",
     'description': "Upload scenes to Previz.",
