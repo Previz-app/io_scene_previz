811ed77ec12b7b6ecbf6006084fc3400f6e5ee37
Charles Flèche <charles@previz.co>
2017-08-22 12:36:45 +0200

    Update test cli test module name to io_scene_previz

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -1,4 +1,4 @@
 SET BLENDER_EXE="C:\Program Files\Blender Foundation\Blender\blender.exe"
 SET TESTS_DIR=%~dp0
 
-%BLENDER_EXE% --factory-startup --debug --background --addons io_scene_dnb_previz --python %TESTS_DIR%run_tests.py
+%BLENDER_EXE% --factory-startup --debug --background --addons io_scene_previz --python %TESTS_DIR%run_tests.py
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,5 +2,5 @@
 
 TESTS_DIR=`dirname ${BASH_SOURCE[0]}`
 
-blender --factory-startup --debug --background --addons io_scene_dnb_previz --python $TESTS_DIR/run_tests.py
+blender --factory-startup --debug --background --addons io_scene_previz --python $TESTS_DIR/run_tests.py
 
